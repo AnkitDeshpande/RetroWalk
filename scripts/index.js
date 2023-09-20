9ec84a8d25310df2ce32b1cc9344da5d38646efa
Ankit Deshpande <urstrulyankit@protonmail.com>
2023-09-20 22:44:10 +0530

    final

--- scripts/index.js
+++ scripts/index.js
@@ -6,108 +6,111 @@ let searchinput = document.querySelector(".search__input");
 let data;
 
 async function getData() {
-  try {
-    const response = await fetch(
-      "https://63c791ce5c0760f69ab9abcc.mockapi.io/product"
-    );
-    data = await response.json();
-    console.log(data);
-    filterdata(data);
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        const response = await fetch(
+            "https://63c791ce5c0760f69ab9abcc.mockapi.io/product"
+        );
+        data = await response.json();
+        console.log(data);
+        filterdata(data);
+    } catch (error) {
+        console.error(error);
+    }
 }
 getData();
 
 filterBy.addEventListener("change", () => {
-  getData();
+    getData();
 });
 function filterdata(data) {
-  let filterValue = filterBy.value;
+    let filterValue = filterBy.value;
 
-  if (filterValue === "") {
-    displayproduct(data);
-  } else {
-    data = data.filter((product) => {
-      //it will return boolean value;
-      return product.category == filterValue;
-    });
-    displayproduct(data);
-  }
+    if (filterValue === "") {
+        displayproduct(data);
+    } else {
+        data = data.filter((product) => {
+            //it will return boolean value;
+            return product.category == filterValue;
+        });
+        displayproduct(data);
+    }
 }
 
 function displayproduct(data) {
-  cont.innerHTML = "";
-  data.forEach((element) => {
-    let cardEl = document.createElement("div");
-    let imgEl = document.createElement("img");
-    let brandEl = document.createElement("h3");
-    let categoryEl = document.createElement("h6");
-    let detailEl = document.createElement("p");
-    let priceEl = document.createElement("h4");
-    let addToCart = document.createElement("button");
+    cont.innerHTML = "";
+    data.forEach((element) => {
+        let cardEl = document.createElement("div");
+        let imgEl = document.createElement("img");
+        let brandEl = document.createElement("h3");
+        let categoryEl = document.createElement("h6");
+        let detailEl = document.createElement("p");
+        let priceEl = document.createElement("h4");
+        let addToCart = document.createElement("button");
 
-    imgEl.src = element.img;
-    brandEl.textContent = element.brand;
-    priceEl.textContent = `₹` + element.price;
-    detailEl.textContent = element.details;
-    categoryEl.textContent = element.category;
-    addToCart.textContent = "Add To Cart";
+        imgEl.src = element.img;
+        brandEl.textContent = element.brand;
+        priceEl.textContent = `₹` + element.price;
+        detailEl.textContent = element.details;
+        categoryEl.textContent = element.category;
+        addToCart.textContent = "Add To Cart";
 
-    // adding events to button "Add to cart" here:-
-    addToCart.addEventListener("click", () => {
-      if (checkduplicate(element)) {
-        alert("Product Already in Cart");
-      } else {
-        cartArr.push({ ...element, quantity: 1 });
-        localStorage.setItem("cart", JSON.stringify(cartArr));
-        alert("Product Added To Cart");
-      }
+        // adding events to button "Add to cart" here:-
+        addToCart.addEventListener("click", () => {
+            if (checkduplicate(element)) {
+                alert("Product Already in Cart");
+            } else {
+                cartArr.push({ ...element, quantity: 1 });
+                localStorage.setItem("cart", JSON.stringify(cartArr));
+                alert("Product Added To Cart");
+            }
+        });
+
+        // appending contents
+        cardEl.append(imgEl, brandEl, priceEl, detailEl, categoryEl, addToCart);
+        cont.append(cardEl);
     });
-
-    // appending contents
-    cardEl.append(imgEl, brandEl, priceEl, detailEl, categoryEl, addToCart);
-    cont.append(cardEl);
-  });
 }
 
 //check if product is already in cart.
 function checkduplicate(product) {
-  for (let i = 0; i < cartArr.length; i++) {
-    if (cartArr[i].id === product.id) {
-      return true;
+    for (let i = 0; i < cartArr.length; i++) {
+        if (cartArr[i].id === product.id) {
+            return true;
+        }
     }
-  }
-  return false;
+    return false;
 }
 
 // searchbar functionality
 searchbtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  let searchParams = searchinput.value;
-  let filtered = data.filter((element) => {
-    if (
-      element.brand.toUpperCase().includes(searchParams.toUpperCase()) === true
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  });
-  displayproduct(filtered);
+    e.preventDefault();
+    let searchParams = searchinput.value;
+    let filtered = data.filter((element) => {
+        if (
+            element.brand.toUpperCase().includes(searchParams.toUpperCase()) ===
+            true
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+    displayproduct(filtered);
 });
 
 // adding hidden div inside category
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
-  button.addEventListener("mouseover", (e) => {
-    content = e.target.querySelector(".btncontent");
-    content.style.display = "block";
-  });
-  button.addEventListener("mouseout", (e) => {
-    if (!e.relatedTarget || !e.relatedTarget.closest("button")) {
-      const content = e.target.querySelector(".btncontent");
-      content.style.display = "none";
-    }
-  });
+    button.addEventListener("mouseover", (e) => {
+        content = e.target.querySelector(".btncontent");
+        content.style.display = "block";
+    });
+    button.addEventListener("mouseout", (e) => {
+        if (!e.relatedTarget || !e.relatedTarget.closest("button")) {
+            const content = e.target.querySelector(".btncontent");
+            content.style.display = "none";
+        }
+    });
 });
+
+// -------------------------
--- scripts/menpage.js
+++ scripts/menpage.js
@@ -1,5 +1,5 @@
 let api =
-  "https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-tech-products";
+    "https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-tech-products";
 let cont = document.getElementById("product-container");
 let filterBy = document.getElementById("filter");
 let cartArr = JSON.parse(localStorage.getItem("cart")) || [];
@@ -8,101 +8,102 @@ let searchinput = document.querySelector(".search__input");
 let data;
 
 async function fetchdata() {
-  try {
-    let res = await fetch(api);
-    res = await res.json();
-    console.log(res);
-    data = res.data;
-    filterdata(data);
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        let res = await fetch(api);
+        res = await res.json();
+        console.log(res);
+        data = res.data;
+        filterdata(data);
+    } catch (error) {
+        console.log(error);
+    }
 }
 fetchdata();
 
 filterBy.addEventListener("change", () => {
-  fetchdata();
+    fetchdata();
 });
 function filterdata(data) {
-  let filterValue = filterBy.value;
+    let filterValue = filterBy.value;
 
-  if (filterValue === "") {
-    displayproduct(data);
-  } else {
-    data = data.filter((product) => {
-      return product.category == filterValue;
-    });
-    displayproduct(data);
-  }
+    if (filterValue === "") {
+        displayproduct(data);
+    } else {
+        data = data.filter((product) => {
+            return product.category == filterValue;
+        });
+        displayproduct(data);
+    }
 }
 
 function displayproduct(data) {
-  cont.innerHTML = "";
-  data.forEach((element) => {
-    let cardEl = document.createElement("div");
-    let imgEl = document.createElement("img");
-    let brandEl = document.createElement("h3");
-    let categoryEl = document.createElement("p");
-    let detailEl = document.createElement("p");
-    let priceEl = document.createElement("h4");
-    let addToCart = document.createElement("button");
+    cont.innerHTML = "";
+    data.forEach((element) => {
+        let cardEl = document.createElement("div");
+        let imgEl = document.createElement("img");
+        let brandEl = document.createElement("h3");
+        let categoryEl = document.createElement("p");
+        let detailEl = document.createElement("p");
+        let priceEl = document.createElement("h4");
+        let addToCart = document.createElement("button");
 
-    imgEl.src = element.img;
-    brandEl.textContent = element.brand;
-    priceEl.textContent = `₹` + element.price;
-    detailEl.textContent = element.details;
-    categoryEl.textContent = element.category;
-    addToCart.textContent = "Add To Cart";
+        imgEl.src = element.img;
+        brandEl.textContent = element.brand;
+        priceEl.textContent = `₹` + element.price;
+        detailEl.textContent = element.details;
+        categoryEl.textContent = element.category;
+        addToCart.textContent = "Add To Cart";
 
-    addToCart.addEventListener("click", () => {
-      if (checkduplicate(element)) {
-        alert("Product Already in Cart");
-      } else {
-        cartArr.push({ ...element, quantity: 1 });
-        localStorage.setItem("cart", JSON.stringify(cartArr));
-        alert("Product Added To Cart");
-      }
+        addToCart.addEventListener("click", () => {
+            if (checkduplicate(element)) {
+                alert("Product Already in Cart");
+            } else {
+                cartArr.push({ ...element, quantity: 1 });
+                localStorage.setItem("cart", JSON.stringify(cartArr));
+                alert("Product Added To Cart");
+            }
+        });
+
+        cardEl.append(imgEl, brandEl, priceEl, detailEl, categoryEl, addToCart);
+        cont.append(cardEl);
     });
-
-    cardEl.append(imgEl, brandEl, priceEl, detailEl, categoryEl, addToCart);
-    cont.append(cardEl);
-  });
 }
 
 function checkduplicate(product) {
-  for (let i = 0; i < cartArr.length; i++) {
-    if (cartArr[i].id === product.id) {
-      return true;
+    for (let i = 0; i < cartArr.length; i++) {
+        if (cartArr[i].id === product.id) {
+            return true;
+        }
     }
-  }
-  return false;
+    return false;
 }
 
 searchbtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  let searchParams = searchinput.value;
-  let filtered = data.filter((element) => {
-    if (
-      element.brand.toUpperCase().includes(searchParams.toUpperCase()) === true
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  });
-  displayproduct(filtered);
+    e.preventDefault();
+    let searchParams = searchinput.value;
+    let filtered = data.filter((element) => {
+        if (
+            element.brand.toUpperCase().includes(searchParams.toUpperCase()) ===
+            true
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+    displayproduct(filtered);
 });
 
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
-  button.addEventListener("mouseover", (e) => {
-    const content = e.target.querySelector(".btncontent");
-    content.style.display = "block";
-  });
-  button.addEventListener("mouseout", (e) => {
-    if (!e.relatedTarget || !e.relatedTarget.closest("button")) {
-      const content = e.target.querySelector(".btncontent");
-      content.style.display = "none";
-    }
-  });
+    button.addEventListener("mouseover", (e) => {
+        const content = e.target.querySelector(".btncontent");
+        content.style.display = "block";
+    });
+    button.addEventListener("mouseout", (e) => {
+        if (!e.relatedTarget || !e.relatedTarget.closest("button")) {
+            const content = e.target.querySelector(".btncontent");
+            content.style.display = "none";
+        }
+    });
 });
